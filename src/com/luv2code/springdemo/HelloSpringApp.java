f3f2e750dd378306788c30852d4bfa76f7485fde
zakumann <samuel742able@gmail.com>
2022-06-19 22:18:31 +0900

    update Spring

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -8,13 +8,16 @@ public class HelloSpringApp {
 
 		//load the spring configuration file
 		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("ApplicationContext.xml");
+				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		
+		// let's call our new method for fortunes
+		System.out.println(theCoach.getDailyFortune());
 		
 		//close the context
 		context.close();
