965e4fd959c079620b99d26bd74f9dd442820e3a
zakumann <samuel742able@gmail.com>
2022-08-19 23:50:59 +0900

    update spring

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/DatabaseFortuneService.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class DatabaseFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
