32074de7a7a34f27c74f83cea63e569f7f037400
zakumann <samuel742able@gmail.com>
2022-09-11 09:41:14 +0900

    add all-java-no-xml

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -5,7 +5,6 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,21 +18,20 @@ public class TennisCoach implements Coach {
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
-	
+
 	// define my init method
 	@PostConstruct
 	public void doMyStartupStuff() {
 		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
 	}
 	
-	//define my destroy method
+	// define my destroy method
 	@PreDestroy
 	public void doMyCleanupStuff() {
-		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");		
 	}
 	
-	
-	
+	// define a setter method
 	/*
 	@Autowired
 	public void setFortuneService(FortuneService theFortuneService) {
