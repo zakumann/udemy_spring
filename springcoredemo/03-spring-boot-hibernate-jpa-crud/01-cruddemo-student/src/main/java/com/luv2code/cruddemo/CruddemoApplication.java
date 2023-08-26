bfed7075e74b1e1784c98bd520a835d22c1f9096
zakumann <samuel742able@gmail.com>
2023-08-26 17:07:46 +0900

    08262023

--- springcoredemo/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ springcoredemo/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args
-		);}
+	public static void main(String[] args) { SpringApplication.run(CruddemoApplication.class, args); }
 
 	@Bean
 	public CommandLineRunner commandLineRunner(String[] args){
